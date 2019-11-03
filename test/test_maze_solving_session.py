f674039a26474626c03c77e893d9c7a311e2c489
k6ps <tanavst@gmail.com>
2019-11-03 12:20:56 +0200

    Added start method to MazeSolvingSession

--- test/test_maze_solving_session.py
+++ test/test_maze_solving_session.py
@@ -5,9 +5,15 @@ from maze_solver.maze_solving_session import MazeSolvingSession
 
 class MazeSolvingSessionTests(unittest.TestCase):
 
+    def setUp(self):
+        self._maze = MagicMock()
+        self._maze.start_square.return_value = {'x': 1, 'y': 1}
+        self._maze_solver = MagicMock()
+        self._maze_solving_session = MazeSolvingSession(maze = self._maze, maze_solver = self._maze_solver)
+
     def test_should_place_maze_solver_in_square_x1_y1_before_start(self):
-        _maze = MagicMock()
-        _maze.start_square.return_value = {'x': 1, 'y': 1}
-        _maze_solver = MagicMock()
-        _maze_solving_session = MazeSolvingSession(maze = _maze, maze_solver = _maze_solver)
-        self.assertEqual({'x': 1, 'y': 1}, _maze_solving_session.current_square)
+        self.assertEqual({'x': 1, 'y': 1}, self._maze_solving_session.current_square)
+
+    def test_should_call_start_on_maze_solver_when_started(self):
+        self._maze_solving_session.start()
+        self._maze_solver.start.assert_called()
