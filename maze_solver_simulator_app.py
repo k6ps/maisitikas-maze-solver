d30a1e8cd7a280a64241b61f0360dd9f16283b82
k6ps <tanavst@gmail.com>
2019-11-20 10:31:30 +0200

    Running experiments with multiple mazes in a row.

--- maze_solver_simulator_app.py
+++ maze_solver_simulator_app.py
@@ -1,41 +1,54 @@
 import statistics
 from maze_solver.maze_solving_session import SimulatorMazeSolvingSession
+from maze_solver.maze import Maze
 from maze_solver.maze_factory import create_robotex_cyprus_2017_maze, create_a_real_16_to_16_beast
 
 
 _SAMPLE_SIZE = 1000
 _TIME_LIMIT_SEC = 300
 
-# maze = create_a_real_16_to_16_beast()
-maze = create_robotex_cyprus_2017_maze()
-_total_move_counts = []
-_total_motion_times = []
-_below_max_time_count = 0
-for _ in range(_SAMPLE_SIZE):
-    simulator_session = SimulatorMazeSolvingSession(
-        maze, 
-        prefer_non_dead_ends_weight = 10,
-        prefer_unvisited_paths_weight = 3,
-        prefer_closer_to_center_weight = 5, 
-        prefer_no_turns_weight = 0,
-        max_moves=999
-    )
-    _results = simulator_session.start()
-    _total_move_counts.append(_results['move_count'])
-    _total_motion_times.append(_results['motion_time'])
-    if _results['motion_time'] < _TIME_LIMIT_SEC:
-        _below_max_time_count += 1
-print('=================================================================')
-print('=== maze={}'.format(maze.name))
-print('=== move count mean={}'.format(statistics.mean(_total_move_counts)))
-print('=== move count median={}'.format(statistics.median(_total_move_counts)))
-print('=== move count stdev={}'.format(statistics.stdev(_total_move_counts)))
-print('=== move count min={}'.format(min(_total_move_counts)))
-print('=== move count max={}'.format(max(_total_move_counts)))
-print('=== motion time mean={}'.format(statistics.mean(_total_motion_times)))
-print('=== motion time median={}'.format(statistics.median(_total_motion_times)))
-print('=== motion time stdev={}'.format(statistics.stdev(_total_motion_times)))
-print('=== motion time min={}'.format(min(_total_motion_times)))
-print('=== motion time max={}'.format(max(_total_motion_times)))
-print('=== probability of solving within time limit={}'.format(_below_max_time_count * 100 / _SAMPLE_SIZE))
-print('=================================================================')
+def perform_experiment(maze: Maze) -> dict:
+    _total_move_counts = []
+    _total_motion_times = []
+    _below_max_time_count = 0
+    for _ in range(_SAMPLE_SIZE):
+        simulator_session = SimulatorMazeSolvingSession(
+            maze, 
+            prefer_non_dead_ends_weight = 10,
+            prefer_unvisited_paths_weight = 3,
+            prefer_closer_to_center_weight = 5, 
+            prefer_no_turns_weight = 0,
+            max_moves=999
+        )
+        _results = simulator_session.start()
+        _total_move_counts.append(_results['move_count'])
+        _total_motion_times.append(_results['motion_time'])
+        if _results['motion_time'] < _TIME_LIMIT_SEC:
+            _below_max_time_count += 1
+    _probability_of_solving_within_time_limit = _below_max_time_count * 100 / _SAMPLE_SIZE
+    return {
+        'maze_name': maze.name,
+        'move_count_mean': statistics.mean(_total_move_counts),
+        'move_count_median': statistics.median(_total_move_counts),
+        'move_count_stdev': statistics.stdev(_total_move_counts),
+        'move_count_min': min(_total_move_counts),
+        'move_count_max': max(_total_move_counts),
+        'motion_time_mean': statistics.mean(_total_motion_times),
+        'motion_time_median': statistics.median(_total_motion_times),
+        'motion_time_stdev': statistics.stdev(_total_motion_times),
+        'motion_time_min': min(_total_motion_times),
+        'motion_time_max': max(_total_motion_times),
+        'probability_of_solving_within_time_limit': _probability_of_solving_within_time_limit
+    }
+
+def print_experiment_series_results(results: dict):
+    for _result in results:
+        print('=================================================================')
+        for _key in _result.keys():
+            print('{}={}'.format(_key, _result[_key]))
+        print('=================================================================')
+
+experiment_results = []
+experiment_results.append(perform_experiment(maze = create_a_real_16_to_16_beast()))
+experiment_results.append(perform_experiment(maze = create_robotex_cyprus_2017_maze()))
+print_experiment_series_results(experiment_results)
