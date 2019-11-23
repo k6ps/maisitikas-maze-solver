a72cc2b6229994c901f22fe6541f28f2115383c6
k6ps <tanavst@gmail.com>
2019-11-23 15:09:47 +0200

    Added my physical test maze (yet to be built) to maze factory.

--- maze_solver/maze_factory.py
+++ maze_solver/maze_factory.py
@@ -64,6 +64,52 @@ def create_6_to_6_maze() -> Maze:
     ]
     return Maze(_squares, name='6-to-6')
 
+def create_kasemetsaresortspa_test_maze() -> Maze:
+    _squares = [
+        MazeSquare(x = 1, y = 1, y_plus = True, is_start = True),
+        MazeSquare(x = 1, y = 2, y_plus = True, y_minus = True),
+        MazeSquare(x = 1, y = 3, x_plus = True, y_plus = True, y_minus = True),
+        MazeSquare(x = 1, y = 4, y_plus = True, y_minus = True),
+        MazeSquare(x = 1, y = 5, y_plus = True, y_minus = True),
+        MazeSquare(x = 1, y = 6, x_plus = True, y_minus = True),
+
+        MazeSquare(x = 2, y = 1, x_plus = True, y_plus = True),
+        MazeSquare(x = 2, y = 2, x_plus = True, y_minus = True),
+        MazeSquare(x = 2, y = 3, y_plus = True, x_minus = True),
+        MazeSquare(x = 2, y = 4, x_plus = True, y_plus = True, y_minus = True),
+        MazeSquare(x = 2, y = 5, y_minus = True),
+        MazeSquare(x = 2, y = 6, x_minus = True),
+
+        MazeSquare(x = 3, y = 1, x_plus = True, x_minus = True),
+        MazeSquare(x = 3, y = 2, y_plus = True, x_minus = True),
+        MazeSquare(x = 3, y = 3, x_plus = True, y_plus = True, y_minus = True),
+        MazeSquare(x = 3, y = 4, y_plus = True, x_minus = True, y_minus = True),
+        MazeSquare(x = 3, y = 5, y_plus = True, y_minus = True),
+        MazeSquare(x = 3, y = 6, x_plus = True, y_minus = True),
+
+        MazeSquare(x = 4, y = 1, x_plus = True, x_minus = True),
+        MazeSquare(x = 4, y = 2, x_plus = True, y_plus = True),
+        MazeSquare(x = 4, y = 3, x_minus = True, y_minus = True),
+        MazeSquare(x = 4, y = 4, y_plus = True, is_finish = True),
+        MazeSquare(x = 4, y = 5, x_plus = True, y_minus = True),
+        MazeSquare(x = 4, y = 6, x_plus = True, x_minus = True),
+
+        MazeSquare(x = 5, y = 1, x_plus = True, y_plus = True, x_minus = True),
+        MazeSquare(x = 5, y = 2, y_plus = True, x_minus = True, y_minus = True),
+        MazeSquare(x = 5, y = 3, x_plus = True, y_minus = True),
+        MazeSquare(x = 5, y = 4, y_plus = True),
+        MazeSquare(x = 5, y = 5, x_plus = True, x_minus = True, y_minus = True),
+        MazeSquare(x = 5, y = 6, x_plus = True, x_minus = True),
+
+        MazeSquare(x = 6, y = 1, y_plus = True, x_minus = True),
+        MazeSquare(x = 6, y = 2, y_plus = True, y_minus = True),
+        MazeSquare(x = 6, y = 3, y_plus = True, x_minus = True, y_minus = True),
+        MazeSquare(x = 6, y = 4, y_minus = True),
+        MazeSquare(x = 6, y = 5, y_plus = True, x_minus = True),
+        MazeSquare(x = 6, y = 6, x_minus = True, y_minus = True)
+    ]
+    return Maze(_squares, name='6-to-6')
+
 def create_a_real_16_to_16_beast() -> Maze:
     _squares = [
         MazeSquare(x = 1, y = 1, y_plus = True, is_start = True),
