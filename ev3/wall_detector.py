2fe143e20a4afc072f404028b48c326d9540da1b
k6ps <tanavst@gmail.com>
2019-11-26 19:04:20 +0200

    A little fix

--- ev3/wall_detector.py
+++ ev3/wall_detector.py
@@ -4,7 +4,7 @@ from ev3.ultrasound_distance_detectors import EV3UltrasoundDistanceDetectors
 
 class EV3WallDetector(WallDetector):
 
-    def __init__(self, distance_sensors: EV3UltrasoundDistanceDetectors):
+    def __init__(self, distance_sensors: EV3UltrasoundDistanceDetectors, **kwargs):
         self._distance_sensors = distance_sensors
         self._distance_treshold_to_decide_wall_is_blocked = KwArgsUtil.kwarg_or_default(6.0, 'distance_treshold_to_decide_wall_is_blocked', **kwargs)
 
