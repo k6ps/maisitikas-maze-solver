2e224c6d695baee7e8feec2b0496663be784d4db
k6ps <tanavst@gmail.com>
2019-11-27 23:00:49 +0200

    Added console logging to EV3 systemtests

--- ev3_systemtest_turn_left.py
+++ ev3_systemtest_turn_left.py
@@ -1,12 +1,21 @@
 #!/usr/bin/python3
 
 import time
+import logging
+import sys
 from ev3.motors import EV3Motors
 from ev3.ultrasound_distance_detectors import EV3UltrasoundDistanceDetectors
 from ev3.gyro import Gyro
 
 
+def set_up_console_logging():
+    console_handler = logging.StreamHandler(sys.stdout)
+    console_handler.setFormatter(logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s'))
+    logging.basicConfig(level=logging.INFO, handlers=[console_handler])
+    logging.getLogger('ev3.motors').setLevel(logging.DEBUG)
+
 if __name__ == "__main__":
+    set_up_console_logging()
     distance_sensors = EV3UltrasoundDistanceDetectors()
     gyro = Gyro()
     motors = EV3Motors(distance_sensors = distance_sensors, gyro = gyro)
