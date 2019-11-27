3767cbdb601dc720c71254c928a383bb78059038
k6ps <tanavst@gmail.com>
2019-11-27 08:44:38 +0200

    Changed default value for wall detection distance.

--- ev3/wall_detector.py
+++ ev3/wall_detector.py
@@ -6,16 +6,19 @@ class EV3WallDetector(WallDetector):
 
     def __init__(self, distance_sensors: EV3UltrasoundDistanceDetectors, **kwargs):
         self._distance_sensors = distance_sensors
-        self._distance_treshold_to_decide_wall_is_blocked = KwArgsUtil.kwarg_or_default(6.0, 'distance_treshold_to_decide_wall_is_blocked', **kwargs)
+        self._distance_treshold_to_decide_wall_is_blocked = KwArgsUtil.kwarg_or_default(9.0, 'distance_treshold_to_decide_wall_is_blocked', **kwargs)
 
     def is_left_blocked(self) -> bool:
         _distance = self._distance_sensors.get_distances()['left']
+        print('DEBUG - EV3WallDetector: left distance = {}'.format(_distance))
         return _distance < self._distance_treshold_to_decide_wall_is_blocked
     
     def is_front_blocked(self) -> bool:
         _distance = self._distance_sensors.get_distances()['front']
+        print('DEBUG - EV3WallDetector: front distance = {}'.format(_distance))
         return _distance < self._distance_treshold_to_decide_wall_is_blocked
     
     def is_right_blocked(self) -> bool:
         _distance = self._distance_sensors.get_distances()['right']
+        print('DEBUG - EV3WallDetector: right distance = {}'.format(_distance))
         return _distance < self._distance_treshold_to_decide_wall_is_blocked
