2bdddaa6de9fc67273bb49f940031ca3b7a1cace
k6ps <tanavst@gmail.com>
2019-11-27 17:59:52 +0200

    Added a small delay before reading distances
    hoping that it prevents ocasionally seeing dead end where it is actually not. Also refactored a bit to reduce duplicate code.

--- ev3/wall_detector.py
+++ ev3/wall_detector.py
@@ -1,3 +1,4 @@
+import time
 from maze_solver.kwargs_util import KwArgsUtil
 from maze_solver.maze_solver import WallDetector
 from ev3.ultrasound_distance_detectors import EV3UltrasoundDistanceDetectors
@@ -8,17 +9,17 @@ class EV3WallDetector(WallDetector):
         self._distance_sensors = distance_sensors
         self._distance_treshold_to_decide_wall_is_blocked = KwArgsUtil.kwarg_or_default(9.0, 'distance_treshold_to_decide_wall_is_blocked', **kwargs)
 
-    def is_left_blocked(self) -> bool:
-        _distance = self._distance_sensors.get_distances()['left']
-        print('DEBUG - EV3WallDetector: left distance = {}'.format(_distance))
+    def _is_direction_blocked(self, direction: str):
+        time.sleep(0.2)
+        _distance = self._distance_sensors.get_distances()[direction]
+        print('DEBUG - EV3WallDetector: {} distance = {}'.format(direction, _distance))
         return _distance < self._distance_treshold_to_decide_wall_is_blocked
+
+    def is_left_blocked(self) -> bool:
+        return self._is_direction_blocked('left')
     
     def is_front_blocked(self) -> bool:
-        _distance = self._distance_sensors.get_distances()['front']
-        print('DEBUG - EV3WallDetector: front distance = {}'.format(_distance))
-        return _distance < self._distance_treshold_to_decide_wall_is_blocked
+        return self._is_direction_blocked('front')
     
     def is_right_blocked(self) -> bool:
-        _distance = self._distance_sensors.get_distances()['right']
-        print('DEBUG - EV3WallDetector: right distance = {}'.format(_distance))
-        return _distance < self._distance_treshold_to_decide_wall_is_blocked
+        return self._is_direction_blocked('right')
