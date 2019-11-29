c20e6a0d0045d7d8ca202e38bdf3e17d0a49a05e
k6ps <tanavst@gmail.com>
2019-11-29 07:15:27 +0200

    Added one more ev3dev2 module to fake.

--- test/ev3/ev3dev_test_util.py
+++ test/ev3/ev3dev_test_util.py
@@ -7,5 +7,6 @@ class Ev3devTestUtil(object):
     @staticmethod
     def create_fake_ev3dev2_module():
         sys.modules["ev3dev2"] = MagicMock()
+        sys.modules["ev3dev2.motor"] = MagicMock()
         sys.modules["ev3dev2.sensor"] = MagicMock()
         sys.modules["ev3dev2.sensor.lego"] = MagicMock()
