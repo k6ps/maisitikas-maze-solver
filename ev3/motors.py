35dbdd51f8d4000e546d16fe00e98f496aca0f23
k6ps <tanavst@gmail.com>
2019-11-30 03:06:57 +0200

    Removed commented code

--- ev3/motors.py
+++ ev3/motors.py
@@ -26,8 +26,6 @@ class EV3Motors(Motors):
         self._motor_pair = MoveSteering(OUTPUT_A, OUTPUT_B)
         self._power_supply = PowerSupply()
         self._position_corrector = PositionCorrector(self._motor_pair, self._gyro, self._distance_sensors)
-        self._steering_correction_motor_degrees = 10
-        self._steering_correction_speed_rpm = 15
         self._motor_degrees_for_90_degree_turn = 131.5
         self._maze_square_length_mm = 180
         self._move_forward_speed_percent = 35
@@ -36,45 +34,14 @@ class EV3Motors(Motors):
         self._wheel_diameter_mm = 56
         self._wheel_circumference_mm = math.pi * self._wheel_diameter_mm
         self._wheelbase_width_mm = 130.2
-        # self._last_time_right_corrected = False
-        # self._last_time_left_corrected = False
 
     def _get_current_time_milliseconds(self):
         return int(round(time.time() * 1000))
 
     def _get_move_square_forward_time_sec(self, front_distance_mm: float, no_of_squares: int = 1):
         _length_mm = self._maze_square_length_mm
-        # if no_of_squares == 1 and front_distance_mm < self._maze_square_length_mm:
-        #     _length_mm = self._maze_square_length_mm - front_distance_mm
         _rotations = (no_of_squares * _length_mm) / self._wheel_circumference_mm
         return _rotations / (self._move_forward_speed_percent / 60)
-
-    # def _should_correct_to_left(self):
-    #     return self._distance_sensors.are_n_last_left_distances_between_x_and_y()
-
-    # def _should_correct_to_right(self):
-    #     return self._distance_sensors.are_n_last_right_distances_between_x_and_y()
-
-    # def _correct_after_move_forward(self):
-    #     if not self._last_time_left_corrected and self._should_correct_to_left():
-    #         self._logger.debug('Correcting to left')
-    #         self._motor_pair.on_for_degrees(
-    #             steering=Steering.LEFT_ON_SPOT.value, 
-    #             speed=SpeedRPM(self._steering_correction_speed_rpm), 
-    #             degrees=self._steering_correction_motor_degrees
-    #         )
-    #         self._last_time_left_corrected = True
-    #     elif not self._last_time_right_corrected and self._should_correct_to_right() :
-    #         self._logger.debug('Correcting to right')
-    #         self._motor_pair.on_for_degrees(
-    #             steering=Steering.RIGHT_ON_SPOT.value, 
-    #             speed=SpeedRPM(self._steering_correction_speed_rpm), 
-    #             degrees=self._steering_correction_motor_degrees
-    #         )
-    #         self._last_time_right_corrected = True
-    #     else:
-    #         self._last_time_right_corrected = False
-    #         self._last_time_left_corrected = False
 
     def move_forward(self, no_of_squares: int = 1):
         self._logger.debug('Move_forward')
@@ -107,22 +74,6 @@ class EV3Motors(Motors):
             _distances_after['front']
         ))
         self._position_corrector.correct_after_move_forward(_distances_before, _angle_before, _distances_after, _angle_after)
-        # self._correct_after_move_forward()
-        # _front_distance = self._distance_sensors.get_distances()['front']
-        # if _front_distance < 6.0 and _front_distance > 3.0:
-        #     _distance_to_compensate_mm = (_front_distance - 3.0) * 10
-        #     if _distance_to_compensate_mm > 5.0:
-        #         self._logger.debug('Need to correct front distance for {} mm'.format(_distance_to_compensate_mm))
-        #         self._motor_pair.on_for_rotations(
-        #             steering=Steering.STRAIGHT.value, 
-        #             speed=SpeedRPM(self._steering_correction_speed_rpm * self._move_forward_speed_factor), 
-        #             rotations=(_distance_to_compensate_mm / self._wheel_circumference_mm),
-        #             brake=True, block=True
-        #         )
-        # _front_distance_remainder = _distances['front'] % 18
-        # self._logger.debug('Front distance remainder={}'.format(_front_distance_remainder))
-        # if _distances['front'] < 3 * 18 and _front_distance_remainder > 4.5 and _front_distance_remainder < 16.5:
-        #     self._logger.debug('Bad front distance remainder. I may be at bad angle')
         self._logger.debug('Move_forward done')
 
     def turn_left(self):
@@ -146,8 +97,6 @@ class EV3Motors(Motors):
             degrees = self._motor_degrees_for_90_degree_turn + _compensating_factor, 
             brake=True, block=True
         )
-        # self._last_time_right_corrected = False
-        # self._last_time_left_corrected = False
         _angle_after = self._gyro.get_orientation()
         self._logger.debug('Gyro angle change={}'.format(_angle_after - _angle_before))
         time.sleep(0.1)
@@ -158,10 +107,6 @@ class EV3Motors(Motors):
             _distances_after['front']
         ))
         self._position_corrector.correct_after_turn_left(_distances_before, _angle_before, _distances_after, _angle_after)
-        # _front_distance_remainder = _distances['front'] % 18
-        # self._logger.debug('Front distance remainder={}'.format(_front_distance_remainder))
-        # if _distances['front'] < 3 * 18 and _front_distance_remainder > 4.5 and _front_distance_remainder < 16.5:
-        #     self._logger.debug('Bad front distance remainder. I may be at bad angle')
         self._logger.debug('turn_left done')
 
     def turn_right(self):
@@ -185,8 +130,6 @@ class EV3Motors(Motors):
             degrees = self._motor_degrees_for_90_degree_turn + _compensating_factor, 
             brake=True, block=True
         )
-        # self._last_time_right_corrected = False
-        # self._last_time_left_corrected = False
         _angle_after = self._gyro.get_orientation()
         self._logger.debug('Gyro angle change={}'.format(_angle_after - _angle_before))
         time.sleep(0.1)
@@ -197,10 +140,6 @@ class EV3Motors(Motors):
             _distances_after['front']
         ))
         self._position_corrector.correct_after_turn_right(_distances_before, _angle_before, _distances_after, _angle_after)
-        # _front_distance_remainder = _distances['front'] % 18
-        # self._logger.debug('Front distance remainder={}'.format(_front_distance_remainder))
-        # if _distances['front'] < 3 * 18 and _front_distance_remainder > 4.5 and _front_distance_remainder < 16.5:
-        #     self._logger.debug('Bad front distance remainder. I may be at bad angle')
         self._logger.debug('turn_right done')
 
     def turn_back(self):
@@ -228,8 +167,6 @@ class EV3Motors(Motors):
             degrees = 2 * (self._motor_degrees_for_90_degree_turn + _compensating_factor), 
             brake=True, block=True
         )
-        # self._last_time_right_corrected = False
-        # self._last_time_left_corrected = False
         _angle_after = self._gyro.get_orientation()
         self._logger.debug('Gyro angle change={}'.format(_angle_after - _angle_before))
         time.sleep(0.1)
@@ -240,10 +177,6 @@ class EV3Motors(Motors):
             _distances_after['front']
         ))
         self._position_corrector.correct_after_turn_back(_distances_before, _angle_before, _distances_after, _angle_after)
-        # _front_distance_remainder = _distances['front'] % 18
-        # self._logger.debug('Front distance remainder={}'.format(_front_distance_remainder))
-        # if _distances['front'] < 3 * 18 and _front_distance_remainder > 4.5 and _front_distance_remainder < 16.5:
-        #     self._logger.debug('Bad front distance remainder. I may be at bad angle')
         self._logger.debug('turn_back done')
 
     def no_turn(self):
