367611eed564ffd5af68b6be36a507bef3775518
k6ps <tanavst@gmail.com>
2019-11-30 04:02:05 +0200

    Removed unused code

--- maze_solver_ev3_app.py
+++ maze_solver_ev3_app.py
@@ -4,14 +4,7 @@ import logging
 import logging.handlers
 import queue
 import sys
-from ev3.motors import EV3Motors
-from ev3.wall_detector import EV3WallDetector
-from ev3.ultrasound_distance_detectors import EV3UltrasoundDistanceDetectors
-from ev3.gyro import Gyro
-from ev3.buttons import EV3Buttons
 from ev3.maze_solver import EV3MazeSolver
-from maze_solver.curious_maze_solver import CuriousMazeSolver
-from maze_solver.maze_solver import FinishDetector, Outputs, NotificationType
 
 
 def set_up_async_file_logging() -> list:
@@ -32,34 +25,16 @@ def set_up_async_file_logging() -> list:
     return [console_log_message_queue_listener, file_log_message_queue_listener]
 
 
-
 if __name__ == "__main__":
     log_message_queue_listeners = set_up_async_file_logging()
     for listener in log_message_queue_listeners:
         listener.start()
     logging.info('maze_solver_ev3_app: starting')
     maze_solver = EV3MazeSolver()
-    # ev3_distance_sensors = EV3UltrasoundDistanceDetectors()
-    # ev3_distance_sensors.start()
-    # ev3_gyro = Gyro()
-    # ev3_gyro.start()
-    # motors = EV3Motors(distance_sensors = ev3_distance_sensors, gyro = ev3_gyro)
-    # wall_detector = EV3WallDetector(distance_sensors = ev3_distance_sensors)
-    # maze_solver = CuriousMazeSolver(
-    #     motors=motors, 
-    #     wall_detector=wall_detector, 
-    #     finish_detector=DummyFinishDetector(), 
-    #     outputs=DummyOutputs
-    # )
-    # ev3_buttons = EV3Buttons()
-    # ev3_buttons.start()
     try:
         maze_solver.start()
     except KeyboardInterrupt:
         maze_solver.stop()
-        # ev3_gyro.stop()
-        # ev3_distance_sensors.stop()
-        # ev3_buttons.stop()
         logging.info('maze_solver_ev3_app: stopped')
         for listener in log_message_queue_listeners:
             listener.stop()
