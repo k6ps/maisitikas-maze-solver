0b4aa76b385e950d4d47d9a601a6807842809b5c
Taavi Tänavsuu <tanavst@gmail.com>
2021-11-04 20:02:35 +0200

    Added logging, testing just one move.

--- ev3_systemtest_move_forward.py
+++ ev3_systemtest_move_forward.py
@@ -1,15 +1,25 @@
 #!/usr/bin/python3
 
 import time
+import logging
+import sys
 from ev3.motors import EV3Motors
 from ev3.distance_detectors import EV3DistanceDetectors
 from ev3.gyro import Gyro
 
+def set_up_console_logging():
+    console_handler = logging.StreamHandler(sys.stdout)
+    console_handler.setFormatter(logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s'))
+    logging.basicConfig(level=logging.INFO, handlers=[console_handler])
+    logging.getLogger('ev3.motors').setLevel(logging.DEBUG)
+    logging.getLogger('ev3.position_corrector').setLevel(logging.DEBUG)
+
 if __name__ == "__main__":
+    set_up_console_logging()
     distance_sensors = EV3DistanceDetectors()
     distance_sensors.start()
     motors = EV3Motors(distance_sensors = distance_sensors, gyro = Gyro())
-    for _ in range(5):
+    for _ in range(1):
         motors.move_forward()
         time.sleep(1)
     distance_sensors.stop()
